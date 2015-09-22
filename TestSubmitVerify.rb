c2b176b9f3a4689b4af0b4c1392403ef2bd336b7
Joe Ricciardi <joetools@gmail.com>
2015-09-22 11:07:21 -0600

    first commit

---
+++ TestSubmitVerify.rb
@@ -0,0 +1,27 @@
+require 'net/http'
+require "uri"
+
+# Constants
+
+TestSubmitRequest_URL = "https://www.nexternal.com/shared/xml/testsubmit.rest"
+TestSubmitRequest_XML = <<-xml
+<?xml version="1.0" encoding="utf-8" ?>
+<TestSubmitRequest>
+  <Credentials @XXX>
+    <AccountName>ACCOUNTNAME</AccountName>
+    <UserName>USERNAME</UserName>
+    <Password>PASSWORD</Password>
+  </Credentials>
+</TestSubmitRequest>
+xml
+
+# Step 1: submit TestSubmitRequest
+
+uri = URI.parse TestSubmitRequest_URL
+request = Net::HTTP::Post.new uri.path
+request.body = TestSubmitRequest_XML
+request.content_type = 'application/xml'
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+response = http.request(request)
+p response.body
